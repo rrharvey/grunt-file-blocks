ee7711936b81a49740e0639c0db3ccfbbe43d17a
Ryan Harvey <rharvey@intertech.com>
2015-05-29 12:28:09 -0500

    Added support for globbing patterns for source files.
    closes #10

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,107 +8,142 @@
 
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        jshint: {
-            all: [
-              'Gruntfile.js',
-              'tasks/*.js',
-              'lib/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc',
-            },
-        },
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        'lib/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
 
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: ['tmp'],
-        },
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
 
-        copy: {
-            dev: {
-                src: 'spec/fixtures/sample.html',
-                dest: 'tmp/sample_dev.html'
-            }
-        },
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['spec/fixtures/sample*.html'],
+          dest: 'tmp'
+        }, {
+          src: ['spec/fixtures/sample.html'],
+          dest: 'tmp/dev.html'
+        }]
+      }
+    },
 
-        // Configuration to be run (and then tested).
-        fileblocks: {
-            options: {
-                removeBlock: false,
-                removeAnchors: false,
-                removeFiles: false,
-                prefix: '',
-                templates: {
-                    raw: '${file}'
-                }
-            },
-            dev: {
-                options: {
-                    prefix: '!!!',
-                    templates: {
-                        raw: '${file},',
-                        ref: '/// <reference data-name="custom" path="${file}" />'
-                    }
-                },
-                files: [
-                    {
-                        options: {
-                            prefix: ''
-                        },
-                        src: ['tmp/sample_dev.html'],
-                        blocks: [
-                            { name: 'styles', src: 'spec/fixtures/css/*.css' },
-                            { name: 'app', src: ['spec/fixtures/js/script1.js', 'spec/fixtures/js/script2.js'], prefix: '~/' },
-                            { name: 'files', src: ['spec/fixtures/js/*.js'] }
-                        ]
-                    },
-                    {
-                        src: 'spec/fixtures/sample.js',
-                        dest: 'tmp/sample_dev.js',
-                        options: {
-                            templates: {
-                                ref: '/// <reference data-custom="hello" path="${file}" />'
-                            }
-                        },
-                        blocks: {
-                            'references': { src: 'spec/fixtures/js/*.js', template: '/// <reference data-custom="local" path="${file}" />' }
-                            }
-                        }
-                    ]
-            },
-            dist: {
-                options: {
-                    removeAnchors: true
-                },
-                src: 'spec/fixtures/sample.html',
-                dest: 'tmp/sample_dist.html',
-                blocks: [
-                    { name: 'styles', src: ['spec/fixtures/css/*.css'] },
-                    { name: 'app', src: ['spec/fixtures/js/script1.js', 'spec/fixtures/js/script2.js'], prefix: '~/' },
-                    { name: 'reload', removeBlock: true }
-                ]
-            }
+    // Configuration to be run (and then tested).
+    fileblocks: {
+      options: {
+        removeBlock: false,
+        removeAnchors: false,
+        removeFiles: false,
+        prefix: '',
+        templates: {
+          raw: '${file}'
         }
-    });
+      },
+      dev: {
+        options: {
+          prefix: '!!!',
+          templates: {
+            raw: '${file},',
+            ref: '/// <reference data-name="custom" path="${file}" />'
+          }
+        },
+        files: [{
+          options: {
+            prefix: ''
+          },
+          src: 'tmp/dev.html',
+          blocks: [{
+            name: 'styles',
+            src: 'spec/fixtures/css/*.css'
+          }, {
+            name: 'app',
+            src: ['spec/fixtures/js/script1.js', 'spec/fixtures/js/script2.js'],
+            prefix: '~/'
+          }, {
+            name: 'files',
+            src: ['spec/fixtures/js/*.js']
+          }]
+        }, {
+          options: {
+            prefix: ''
+          },
+          src: ['tmp/sample*.html'],
+          blocks: [{
+            name: 'styles',
+            src: 'spec/fixtures/css/*.css'
+          }, {
+            name: 'app',
+            src: ['spec/fixtures/js/script1.js', 'spec/fixtures/js/script2.js'],
+            prefix: '~/'
+          }, {
+            name: 'files',
+            src: ['spec/fixtures/js/*.js']
+          }]
+        }, {
+          src: 'spec/fixtures/sample.js',
+          dest: 'tmp/sample_dev.js',
+          options: {
+            templates: {
+              ref: '/// <reference data-custom="hello" path="${file}" />'
+            }
+          },
+          blocks: {
+            'references': {
+              src: 'spec/fixtures/js/*.js',
+              template: '/// <reference data-custom="local" path="${file}" />'
+            }
+          }
+        }]
+      },
+      dist: {
+        options: {
+          removeAnchors: true
+        },
+        src: 'spec/fixtures/sample.html',
+        dest: 'tmp/sample_dist.html',
+        blocks: [{
+          name: 'styles',
+          src: ['spec/fixtures/css/*.css']
+        }, {
+          name: 'app',
+          src: ['spec/fixtures/js/script1.js', 'spec/fixtures/js/script2.js'],
+          prefix: '~/'
+        }, {
+          name: 'reload',
+          removeBlock: true
+        }]
+      }
+    }
+  });
 
-    // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-jasmine-node');
-    grunt.loadNpmTasks('grunt-bump');
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-bump');
 
-    grunt.registerTask('debug', ['clean', 'copy', 'fileblocks:dev']);
+  grunt.registerTask('debug', ['clean', 'copy', 'fileblocks:dev']);
 
-    grunt.registerTask('test', ['jasmine_node']);
+  grunt.registerTask('test', ['jasmine_node']);
 
-    grunt.registerTask('default', ['debug']);
+  grunt.registerTask('default', ['debug']);
 };
--- tasks/fileblocks.js
+++ tasks/fileblocks.js
@@ -14,115 +14,108 @@ var BlockConfig = require('../lib/blockconfig');
 var grunt = require('grunt');
 var _ = require('lodash');
 
-module.exports = function (grunt) {
-    var TASKNAME = 'fileblocks';
+module.exports = function(grunt) {
+  var TASKNAME = 'fileblocks';
 
-    /**
-     * Normalize and return block configurations from the Gruntfile.
-     * @param {Object[]|Object.<string, object>} blocks - The block configurations from the Gruntfile.
-     * @returns {BlockConfig[]}
-     */
-    var getConfigs = function (data, options) {
-        var configs = [];
+  /**
+   * Normalize and return block configurations from the Gruntfile.
+   * @param {Object[]|Object.<string, object>} blocks - The block configurations from the Gruntfile.
+   * @returns {BlockConfig[]}
+   */
+  var getConfigs = function(data, options) {
+    var configs = [];
 
-        if (_.isArray(data)) {
-            data.forEach(function (block) {
-                configs.push(new BlockConfig(block.name, block, options));
-            });
-        } else if (_.isPlainObject(data)) {
-            _.forOwn(data, function (value, name) {
-                configs.push(new BlockConfig(name, value, options));
-            });
-        } else {
-            grunt.warn('Block configuration must be an array or object.');
-        }
+    if (_.isArray(data)) {
+      data.forEach(function(block) {
+        configs.push(new BlockConfig(block.name, block, options));
+      });
+    } else if (_.isPlainObject(data)) {
+      _.forOwn(data, function(value, name) {
+        configs.push(new BlockConfig(name, value, options));
+      });
+    } else {
+      grunt.warn('Block configuration must be an array or object.');
+    }
 
-        return configs;
+    return configs;
+  };
+
+  /**
+   * Validate the source-destination file mapping.
+   * @param {Object} A source-destination file mapping.
+   */
+  var validateFile = function(file) {
+    if (file.src.length === 0) {
+      grunt.warn('Source files not found for pattern "' + file.orig.src[0] + '".');
+    }
+  };
+
+  grunt.registerMultiTask(TASKNAME, 'Prepares a block in a file by inserting or removing a line (script tag, link, or reference) for each file matching a specified pattern.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var defaultOptions = {
+      removeBlock: false,
+      removeAnchors: false,
+      removeFiles: false,
+      rebuild: false,
+      templates: {
+        'js': '<script src="${file}"></script>',
+        'css': '<link href="${file}" rel="stylesheet" />',
+        'ref': '/// <reference path="${file}" />',
+        'raw': '${file}'
+      },
+      templatesFn: {}
     };
 
-    /**
-     * Validate the source-destination file mapping.
-     * @param {Object} A source-destination file mapping.
-     */
-    var validateFile = function (file) {
-        if (file.src.length > 1) {
-            var ignored = file.src.splice(1);
-            grunt.log.errorlns('Expected a single source file. Ignoring ' + ignored.join(', ') + '.');
+    var options = _.merge(defaultOptions, this.options());
+
+    this.files.forEach(function(file) {
+      validateFile(file);
+
+      file.src.forEach(function(srcPath) {
+        var destPath = file.dest;
+
+        if (!!!file.blocks) {
+          grunt.warn('No blocks configured for ' + srcPath);
         }
 
-        if (!file.src[0]) {
-            grunt.warn('Source files not found for pattern "' + file.orig.src[0] + '".');
+        // There are blocks are defined
+        var targetOpts = _.clone(options);
+        _.merge(targetOpts, file.options);
+        var configs = getConfigs(file.blocks, targetOpts);
+
+        if (configs.length === 0) {
+          grunt.warn('No blocks configured for ' + srcPath);
         }
 
-        if (!grunt.file.exists(file.src[0])) {
-            grunt.warn('Source file ' + file.src[0] + ' not found.');
-        }
-    };
+        var srcFile = new File(srcPath).load();
+        var processor = new FileProcessor(srcFile);
+        var blocks = processor.getBlocks(configs);
 
-    grunt.registerMultiTask(TASKNAME, 'Prepares a block in a file by inserting or removing a line (script tag, link, or reference) for each file matching a specified pattern.', function () {
-        // Merge task-specific and/or target-specific options with these defaults.
-        var defaultOptions = {
-            removeBlock: false,
-            removeAnchors: false,
-            removeFiles: false,
-            rebuild: false,
-            templates: {
-                'js': '<script src="${file}"></script>',
-                'css': '<link href="${file}" rel="stylesheet" />',
-                'ref': '/// <reference path="${file}" />',
-                'raw': '${file}'
-            },
-            templatesFn: {}
-        };
+        grunt.log.debug('Source file before processing.');
+        grunt.log.debug(srcFile.content);
 
-        var options = _.merge(defaultOptions, this.options());
+        blocks.forEach(function(block) {
 
-        this.files.forEach(function (file) {
-            validateFile(file);
-            var srcPath = file.src[0];
-            var destPath = file.dest;
+          if (block.config.rebuild) {
+            block.rebuildFiles();
+          } else {
+            block.updateFiles();
+          }
 
-            if (!!!file.blocks){
-                grunt.warn('No blocks configured for ' + srcPath);
-            }
-
-            // There are blocks are defined
-            var targetOpts = _.clone(options);
-            _.merge(targetOpts, file.options);
-            var configs = getConfigs(file.blocks, targetOpts);
-
-            if (configs.length === 0){
-                grunt.warn('No blocks configured for ' + srcPath);
-            }
-
-            var srcFile = new File(srcPath).load();
-            var processor = new FileProcessor(srcFile);
-            var blocks = processor.getBlocks(configs);
-
-            grunt.log.debug('Source file before processing.');
-            grunt.log.debug(srcFile.content);
-
-            blocks.forEach(function (block) {
-
-                if (block.config.rebuild) {
-                    block.rebuildFiles();
-                } else {
-                    block.updateFiles();
-                }
-
-                processor.processBlock(block);
-            });
-
-            grunt.log.debug('Source file after processing.');
-            grunt.log.debug(srcFile.content);
-
-            var updatedBlocks = blocks.filter(function (block) {
-                return block.changed;
-            });
-
-            if (updatedBlocks.length > 0) {
-                srcFile.save(destPath);
-            }
+          processor.processBlock(block);
         });
+
+        grunt.log.debug('Source file after processing.');
+        grunt.log.debug(srcFile.content);
+
+        var updatedBlocks = blocks.filter(function(block) {
+          return block.changed;
+        });
+
+        if (updatedBlocks.length > 0) {
+          srcFile.save(destPath);
+        }
+      });
     });
+  });
 };
